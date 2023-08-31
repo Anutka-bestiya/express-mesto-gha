a653d2dd3acb0989e4e741a2cf987e210a36864b
Anutka_bestiya <anchikfyz@yandex.ru>
2023-09-01 03:40:57 +0500

    14pr refactor valid

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,25 @@
 const routerAuth = require('express').Router(); // создали роутер
+const { celebrate, Joi } = require('celebrate');
 const {
   createUser,
   login,
 } = require('../controllers/users');
+const { LINK_REGEX, PASS_REGEX } = require('../utils/regex');
 
-routerAuth.post('/signup', createUser); // регистрация
-routerAuth.post('/signin', login); // авторизация
+routerAuth.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(LINK_REGEX),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(2).regex(PASS_REGEX),
+  }),
+}), createUser); // регистрация
+routerAuth.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(2).regex(PASS_REGEX),
+  }),
+}), login); // авторизация
 
 module.exports = routerAuth;
--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const routerCards = require('express').Router(); // создали роутер
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards,
   createCard,
@@ -6,11 +7,29 @@ const {
   addLikeCard,
   deleteLikeCard,
 } = require('../controllers/cards');
+const { LINK_REGEX } = require('../utils/regex');
 
 routerCards.get('/', getCards); // GET / cards — возвращает все карточки
-routerCards.post('/', createCard); // POST /cards — создаёт карточку
-routerCards.delete('/:cardId', deleteCard); // DELETE /cards/:cardId — удаляет карточку по идентификатору
-routerCards.put('/:cardId/likes', addLikeCard); // PUT /cards/:cardId/likes — поставить лайк карточке
-routerCards.delete('/:cardId/likes', deleteLikeCard); // DELETE /cards/:cardId/likes — убрать лайк с карточки
+routerCards.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required().regex(LINK_REGEX),
+  }),
+}), createCard); // POST /cards — создаёт карточку
+routerCards.delete('/:cardId', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), deleteCard); // DELETE /cards/:cardId — удаляет карточку по идентификатору
+routerCards.put('/:cardId/likes', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), addLikeCard); // PUT /cards/:cardId/likes — поставить лайк карточке
+routerCards.delete('/:cardId/likes', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), deleteLikeCard); // DELETE /cards/:cardId/likes — убрать лайк с карточки
 
 module.exports = routerCards;
